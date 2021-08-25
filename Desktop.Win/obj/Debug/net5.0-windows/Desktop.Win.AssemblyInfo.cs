edd044410e929bd4bda026e051f3fe418c732b63
aruntomy98 <aruntomy@outlook.com>
2021-08-25 09:30:19 +0530

    RmFi

--- Desktop.Win/obj/Debug/net5.0-windows/Desktop.Win.AssemblyInfo.cs
+++ Desktop.Win/obj/Debug/net5.0-windows/Desktop.Win.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Remotely Desktop")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Remotely_Desktop")]
+[assembly: System.Reflection.AssemblyTitleAttribute("RmFi_Desktop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
